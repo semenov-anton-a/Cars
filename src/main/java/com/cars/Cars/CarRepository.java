015009be30628ea5391b902c7fab0209ec06c5c7
Anton Semenov <semenov.anton.a@gmail.com>
2022-01-19 17:51:22 +0200

    IDEA

--- src/main/java/com/cars/Cars/CarRepository.java
+++ src/main/java/com/cars/Cars/CarRepository.java
@@ -2,8 +2,8 @@ package com.cars.Cars;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CarRepository extends JpaRepository<Car, Long> {
-    List<Car> findByIdNotNull();
-}
+@Repository
+public interface CarRepository extends CrudRepository<Car, Long> {}
--- src/main/java/com/cars/Cars/ClientRepository.java
+++ src/main/java/com/cars/Cars/ClientRepository.java
@@ -1,5 +1,7 @@
 package com.cars.Cars;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ClientRepository extends JpaRepository<Client, Long> {}
+@Repository
+public interface ClientRepository extends CrudRepository<Client, Long> {}
